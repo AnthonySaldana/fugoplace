4ae81ce546b10f1fb331da05db589d258d6dbfd7
Anthony saldana <anthonywebsol@gmail.com>
2016-06-22 22:46:59 -0700

    Show school url in dashboard

--- resources/views/user/includes/userheader.blade.php
+++ resources/views/user/includes/userheader.blade.php
@@ -7,7 +7,12 @@
 <li><a href="{{ action('FolderController@index') }}">Folder Fav</a></li>
 <li><a href="{{ action('TutorialController@index') }}">Tutorials</a></li>
 <br/>
-<li>Your School slug is: </li>
+
+<?php $link = $_SERVER['HTTP_HOST'] . '/school/' . Auth::user()->school_slug; ?>
+
+
 <li><a href="/"><small>Home</small></a></li>
 <li><a href="/logout"><small>Log Out</small></a></li>
+<br/>
+<li>Your School link is: <a href="http://{{ $link }}"> {{ $link }} </a> </li>
 </ul>
