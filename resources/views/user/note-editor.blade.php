453f875c899792cf2a502116a2ace61177ca8a04
Anthony saldana <anthonywebsol@gmail.com>
2016-06-28 22:24:17 -0700

    Note Editor Textarea styling
    - stretched textarea to be over the borders of the page style

--- resources/views/user/note-editor.blade.php
+++ resources/views/user/note-editor.blade.php
@@ -5,38 +5,42 @@
   <link rel="stylesheet" href="{{ URL::asset('css/user.css') }}">
   </head>
   <body id="note-editor">
+    <script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
+    <script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);
+    </script>
+    <script>
+      function myFunction() {
+        confirm("Trash this note?");
+      }
+
+      
+    </script>
+
   @include('user.includes.userheader')
+
   <?php if( isset($note_id) ){ ?>
   <h2> Form #: <?php echo $note_id;  ?></h2>
   <?php } ?>
-
   <section class="paper">
   @include('common.errors')
-
  <!--<article class="head">
     <form class="two" method="get" action="#"><button class="two" type="submit">Save</button></form>
 	<form class="three" method="get" action="#"><button class="two" type="submit">Edit</button></form>
 	<form class="four" method="get" action="#"><button class="two" type="submit" onclick="myFunction()">Discard</button></form>
  </article>-->
-  <script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
-<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
- <script>
-function myFunction() {
-    confirm("Trash this note?");
-}
-</script>
   <form method="post" action="{{ url('/user/notes') }}">
-   <article class="head">
+  <article class="head">
     <button class="two" type="submit" name="send" value="1">Save</button>
-  <!--<button class="two" type="submit" name="edit" value="1">Edit</button>-->
-  <button class="two" type="submit" name="delete" value="1" onclick="myFunction()">Discard</button>
- </article>
-   <label for="title">Enter a title</label>
+    <!--<button class="two" type="submit" name="edit" value="1">Edit</button>-->
+    <button class="two" type="submit" name="delete" value="1" onclick="myFunction()">Discard</button>
+  </article>
+  <div style="position:absolute; top: 98px; width:100%;left: 37px;">
+    <label for="title">Enter a title</label>
     <input type="text" name="title" value="<?php if(isset( $note )){ echo $note[0]['title']; }?>"/>
     <br/>
-
-    <label for="content">Make a note</label>
-    <textarea name="content" placeholder="Enter your note here" style="min-height:800px;"><?php if(isset( $note )){ echo $note[0]['content']; } ?></textarea>
+    <textarea name="content" placeholder="Enter your note here" style="min-height:913px;"><?php if(isset( $note )){ echo $note[0]['content']; } ?></textarea>
+  </div>
+   
     <?php if(isset( $note )){ ?>
         <input type="hidden" name="noteid" value="<?php echo $note[0]['id']; ?>" />
     <?php } ?>
@@ -44,5 +48,15 @@ function myFunction() {
     {{ csrf_field() }}
   </form>   
 </section>
+
+<!--<script>
+setTimeout(1000);
+var nicEditArray = document.getElementsByClassName("nicEdit-main");
+console.log( nicEditArray );
+      var nicEdit = nicEditArray;
+      var nicEditParentDiv = nicEdit.parentNode;
+      console.log()
+      nicEditParentDiv.style.border = 'none !important';
+    </script>-->
   </body>
 </html>
