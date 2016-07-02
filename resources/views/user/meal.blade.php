d373704576e06ee4a81555a3a343507724feaafc
Anthony saldana <anthonywebsol@gmail.com>
2016-07-02 00:22:55 -0700

    Recipe Media Alternator
    - Added radio buttons to allow alternating between available uploaded
    media and the embed video

--- resources/views/user/meal.blade.php
+++ resources/views/user/meal.blade.php
@@ -3,10 +3,42 @@
   <head>
     <title>FugoPlace</title>
     <link rel="stylesheet" href="{{ URL::asset('css/user.css') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <style type="text/css">
+        .meal-media, .meal-video{
+          margin-top:15px;
+        }
+
+        .delete-form, .btn.edit{
+          padding-right:10px;
+        }
+    </style>
   </head>
   <body id="meal">
     @include('user.includes.userheader')
     <script>
+    $( document ).ready(function() {
+       var mediatype = $( 'input[type=radio][name=media_selector]' ).val();
+       fugochosemedia( mediatype );
+
+       $('input[type=radio][name=media_selector]').change(function() {
+
+            mediatype = $( this ).val();
+            fugochosemedia( mediatype );
+            
+        });
+    });
+
+    function fugochosemedia( mediatype ){
+        if( 'M' == mediatype ){
+          $('.meal-media').show();
+          $('.meal-video').hide();
+        }else if( 'V' == mediatype ){
+          $('.meal-video').show();
+          $('.meal-media').hide();
+        }
+    }
+
 		function myFunction() {
 		    confirm("Trash composed recipe?");
 		}
@@ -44,15 +76,39 @@
   </div>
 
   <div class="right" >
-  <a href="{{ action('RecipeController@edit', array($id) ) }}" class='btn'>Edit</a>
-      <form action="{{ action('RecipeController@destroy', array($id) ) }}" method="post" style="display:inline-block;">
-        <input type="hidden" name="id" value="{{ $id }}" />
-        <input name="_method" type="hidden" value="delete" />
-        <button type="submit" value="delete" class='btn' onclick="return myFunction()">Delete</button>
-        {{ csrf_field() }}
-      </form>
-      <br/><br/>
-	<?php if( ( isset( $recipe['videolink'] ) && !empty( $recipe['videolink'] ) ) && ( isset( $recipe['video_recipe'] ) && 1 == $recipe['video_recipe'] ) ){
+    <a href="{{ action('RecipeController@edit', array($id) ) }}" class='btn edit'>Edit</a>
+    <form action="{{ action('RecipeController@destroy', array($id) ) }}" class='delete-form' method="post" style="display:inline-block;">
+      <input type="hidden" name="id" value="{{ $id }}" />
+      <input name="_method" type="hidden" value="delete" />
+      <button type="submit" value="delete" class='btn' onclick="return myFunction()">Delete</button>
+      {{ csrf_field() }}
+    </form>
+
+    <?php if( ( isset( $recipe['videolink'] ) && !empty( $recipe['videolink'] ) ) && !empty( $media ) ){
+        ?>
+          <small><b>Chose Recipe Format: </b></small>
+          <input type="radio" name="media_selector" class="media_selector" value="M" checked> Uploaded Media
+          <input type="radio" name="media_selector" class="media_selector"  value="V"> Video
+
+          <div class="meal-video" > <iframe width="600" height="400" src="{{ $recipe['videolink'] }}" frameborder="0" allowfullscreen>Please wait.</iframe> </div>
+
+          <div class="meal-media" >
+            
+            <?php if( false == $isvideo ){ ?> 
+              <img src="{{ url('/images/recipes/' . $media) }}"width="600" height="400"/> <?php
+
+              }else{ ?>
+                <video width="600" height="400" controls>
+                  <source src="{{ url('/images/recipes/' . $media) }}" type="video/{{ $ext }}">
+                  Your browser does not support HTML5 video.
+                </video> <?php
+                } 
+
+            ?>
+
+          </div>
+        <?php
+      } else if( ( isset( $recipe['videolink'] ) && !empty( $recipe['videolink'] ) ) && ( isset( $recipe['video_recipe'] ) && 1 == $recipe['video_recipe'] ) ){
 
           ?>
           <iframe width="600" height="400" src="{{ $recipe['videolink'] }}" frameborder="0" allowfullscreen>Please wait.</iframe>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
