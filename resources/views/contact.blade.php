43be5e4428c9129af46520e9545667ef685cc19d
Anthony saldana <anthonywebsol@gmail.com>
2016-07-07 10:01:53 -0700

    contact page fixes
    - aligned elements on page better

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,17 +3,31 @@
   <head>
     <title>FugoPlace</title>
 	@include('includes.head')
+  <style type="text/css">
+  form.elegant-aero{ 
+    margin-left: 0;
+    margin-right: 0;
+    min-width: 500px;
+    float:left;
+   }
+
+   .contact p.one{
+    float:left;
+    position: static;
+   }
+
+   .contact span.one{
+    position: static;
+   }
+  </style>
   </head>
   @include('includes.head')
   <body class="contact">
   @include('includes.header')
     <div></div>
 	<br />
-	<p class="one"><span class="one"><u>Contact Fugo Studio</u></span>
-				<br /> <br />
-			If you have any questions or concerns we would love to hear from you. We are a young company and are eager to improve your experience, so please send us any suggestions. You can contact us through email, as you see to your right, or via phone call; (949)999-999. <br /><strong>Office hours:</strong> Monday-Saturday 9AM-5PM. Fugo Studio is located at 130 Chapman Ave, Fullerton, CA 92832.</p>
-	
-	<form action="" method="post" class="elegant-aero">
+
+    <form action="" method="post" class="elegant-aero">
     <h1>Contact Us
         <span>Please fill all the texts in the fields.</span>
     </h1>
@@ -33,9 +47,9 @@
     </label>
      <label>
         <span>Subject :</span><select name="selection">
-		<option value="General Question">General Question</option>
+    <option value="General Question">General Question</option>
         <option value="Efficiency Concern">Efficiency Concern</option>
-		<option value="Other">Other</option>
+    <option value="Other">Other</option>
         </select>
     </label>    
      <label>
@@ -43,6 +57,9 @@
         <input type="button" class="button" value="Send" />
     </label>    
 </form>
+	<p class="one"><span class="one"><u>Contact Fugo Studio</u></span>
+				<br /> <br />
+			If you have any questions or concerns we would love to hear from you. We are a young company and are eager to improve your experience, so please send us any suggestions. You can contact us through email, as you see to your right, or via phone call; (949)999-999. <br /><strong>Office hours:</strong> Monday-Saturday 9AM-5PM. Fugo Studio is located at 130 Chapman Ave, Fullerton, CA 92832.</p>
 
   </body>
 </html>
