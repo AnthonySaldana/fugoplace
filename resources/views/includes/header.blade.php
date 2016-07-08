e31bddd6e9ddb9b0e5fd3cf68284530420311bc5
Anthony saldana <anthonywebsol@gmail.com>
2016-07-08 16:16:26 -0700

    header too big on some browsers
    - an unnecessary span tag was causing header problems on firefox

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
 	<div class="header">
 		<div class="container"></div>
 			<ul class="nav">
-				<li><a href="/"><span class="two">Fugo</span>&nbsp;&nbsp;<u>Place</u><br /><span></span></a></li>
+				<li><a href="/"><span class="two">Fugo</span>&nbsp;&nbsp;<u>Place</u><br /></a></li>
 				<li><a href="/">Home</a></li>
 				<li><a href="/about">About Us</a></li>
 				<li><a href="/faq">FAQ</a></li>
