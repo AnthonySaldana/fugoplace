4ea62a846d5533184614dfc36f2b6cc4c1c27263
Anthony saldana <anthonywebsol@gmail.com>
2016-07-10 14:39:17 -0700

    change home to school page
    change home in nav to point to school page

--- resources/views/school/includes/header.blade.php
+++ resources/views/school/includes/header.blade.php
@@ -3,11 +3,11 @@
 		<div class="container"></div>
 			<ul class="nav">
 				<li style="position:absolute; left:43%;"><a href="/school/<?php echo $school_link; ?>/fugo"><span class="two" style="position:static;">Fugo</span>&nbsp;&nbsp;<u style="position:static;">Place</u><br /></a></li>
-				<li><a href="/school/<?php echo $school_link; ?>/fugo">Home</a></li>
+				<li><a href="/school/<?php echo $school_link; ?>">Home</a></li>
 				<li><a href="/school/<?php echo $school_link; ?>/about">About Us</a></li>
 				<li><a href="/school/<?php echo $school_link; ?>/faq">FAQ</a></li>
 				<li><a href="/school/<?php echo $school_link; ?>/contact">Contact Us</a></li>
-				<li><a href="/school/<?php echo $school_link; ?>">School</a></li>
+				
 
 				<?php if( true == $isuser ){ ?>
 				<li><a href="/user">user</a></li>
