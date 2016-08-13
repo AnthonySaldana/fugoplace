594dff846f5c816a5bb76a2cedea5b1ad43fa078
Anthony saldana <anthonywebsol@gmail.com>
2016-08-13 03:09:25 -0700

    Specify the 'users' table in User Model

--- app/User.php
+++ app/User.php
@@ -6,6 +6,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
