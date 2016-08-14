1c472ba97b21c34b919b16523c6faa997b63facb
Anthony saldana <anthonywebsol@gmail.com>
2016-08-13 17:29:04 -0700

    New Model For Settings Table
    - Created a new model for the settings table.

---
+++ app/Settings.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Settings extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'settings';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'meta_key', 'meta_value'
+    ];
+}
